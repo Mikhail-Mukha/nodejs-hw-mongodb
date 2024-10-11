dc9e251197199eaf134bae2a8742cfb681396f84
Mikhail-Mukha <misha500krut@gmail.com>
2024-10-11 23:32:32 +0300

    finaly-corrected-commit

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -6,7 +6,6 @@ const userSchema = new Schema(
     password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     avatarUrl: { type: String },
-    // role: { type: String, required: true, enum: ['home', 'personal', 'work'] },
   },
   {
     timestamps: true,
