22de2706574698f60884bc007d179863e36ea675
Mikhail-Mukha <misha500krut@gmail.com>
2024-10-27 02:26:14 +0300

    finaly-correct

--- src/validation/createContactValidationSchema.js
+++ src/validation/createContactValidationSchema.js
@@ -4,7 +4,6 @@ export const createContactSchemaValidation = Joi.object({
   name: Joi.string().required().min(3).max(20),
   phoneNumber: Joi.string().required().min(3).max(20),
   email: Joi.string().required().email(),
-  isFavourite: Joi.boolean().default(false),
+  isFavourite: Joi.boolean().truthy('true').falsy('false'),
   contactType: Joi.string().required().valid('home', 'personal', 'work'),
-  avatarUrl: Joi.string().optional(),
 });
